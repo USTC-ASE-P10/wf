743e9a65402431f56ed82ee44b37d6073768029c
GGchencan <32196669+GGchencan@users.noreply.github.com>
2018-10-30 13:21:15 +0800

    finish step1

--- tests.py
+++ tests.py
@@ -111,5 +111,41 @@ class TestWordOccurrences(unittest.TestCase):
         self.assertEqual(output, expect)
 
 
+class TestLimit(unittest.TestCase):
+    @staticmethod
+    def __run(limit, *files):
+        """
+        Run `wf.py -f -n files` and return the output.
+        :param files: list of file contents.
+        :return: Program output with every file path replaced with a `#`.
+        """
+        tempfiles = []
+        for file in files:
+            tempfile = NamedTemporaryFile('w', delete=False, encoding='utf8')
+            tempfile.write(file)
+            tempfile.flush()
+            tempfiles.append(tempfile)
+        output = ''.join(main(['-f', '-n', str(limit)] + [tempfile.name for tempfile in tempfiles]))
+        for tempfile in tempfiles:
+            output = output.replace(tempfile.name, '#')
+            tempfile.close()
+        return output
+
+    @staticmethod
+    def __format(data, limit):
+        """
+        Format program output.
+        :param data: list of (item, count)s.
+        :return: str to be compared with.
+        """
+        return 'File: #\n' + ''.join('%40s\t%d\n' % i for i in data[:limit]) + '\n'
+
+    def test_file(self):
+        for limit in range(3):
+            with self.subTest(limit=limit):
+                output=self.__run(limit, '1', 'a', 'a b')
+                expect=self.__format([], limit) + self.__format([('a',1)], limit) + self.__format([('a', 1), ('b', 1)], limit)
+                self.assertEqual(output, expect)
+   
 if __name__ == '__main__':
     unittest.main()
--- wf.py
+++ wf.py
@@ -2,6 +2,7 @@
 
 import argparse
 import re
+import os
 from collections import defaultdict
 
 
@@ -21,7 +22,7 @@ def letter(path):
     yield '\n'
 
     
-def word(path):
+def word(path, limit):
     counts = defaultdict(lambda: 0)
     with open(path, encoding='utf8') as f:
         pattern = re.compile(r'[a-zA-Z0-9]+')  
@@ -33,26 +34,46 @@ def word(path):
             w = w.lower()
             counts[w] += 1
     yield 'File: %s\n' %path
-    for (k, v) in sorted(counts.items(), key=lambda i: (-i[1], i[0])):
+    result=sorted(counts.items(), key=lambda i: (-i[1], i[0]))
+    if limit is not None:
+        result = result[:limit]
+    for (k, v) in result:
         yield '%40s\t%d\n' % (k, v)
     yield '\n' 
 
         
-        
-
-
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('paths', metavar='<path>', nargs='+', help='file to process')
     parser.add_argument('-c', dest='function', action='store_const', const='letter', help='count letter occurrences')
     parser.add_argument('-f', dest='function', action='store_const', const='word', help='count word occurrences')
+    parser.add_argument('-d', dest='function', action='store_const', const='dir_word', help='count word occurrences in specified directory')
+    parser.add_argument('-s', dest='recursive', action='store_const', const=True, default=False, help='recursive')
+    parser.add_argument('-n', dest='limit', metavar='N', type=int, help='output top N lines')
     args = parser.parse_args(args)
     if args.function == 'letter':
         for path in args.paths:
             yield from letter(path)
     elif args.function == 'word':
         for path in args.paths:
-            yield from word(path)
+            yield from word(path, args.limit)
+    elif args.function == 'dir_word':
+        if not args.recursive:
+            for path in args.paths:
+                result = os.listdir(path)
+                for r in result:
+                    temp_path = os.path.join(path, r)
+                    if os.path.isdir(temp_path):
+                        continue
+                    yield from word(temp_path, args.limit)
+        else:
+            for path in args.paths:
+                for (p, _, l) in os.walk(path):
+                    for i in l:
+                        yield from word(os.path.join(p, i), args.limit)
+                        
+
+    
             
 
 
