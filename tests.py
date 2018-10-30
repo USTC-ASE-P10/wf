b30264d6f1723a64bcfe74614f4b04a838595e40
Liu-Ze <liuze@mail.ustc.edu.cn>
2018-10-30 14:58:19 +0800

    add step 2

--- tests.py
+++ tests.py
@@ -143,9 +143,51 @@ class TestLimit(unittest.TestCase):
     def test_file(self):
         for limit in range(3):
             with self.subTest(limit=limit):
-                output=self.__run(limit, '1', 'a', 'a b')
-                expect=self.__format([], limit) + self.__format([('a',1)], limit) + self.__format([('a', 1), ('b', 1)], limit)
+                output = self.__run(limit, '1', 'a', 'a b')
+                expect = self.__format([], limit) + self.__format([('a', 1)], limit) + self.__format(
+                    [('a', 1), ('b', 1)], limit)
                 self.assertEqual(output, expect)
-   
+
+
+class TestStopwords(unittest.TestCase):
+    @staticmethod
+    def __run(*files):
+        """
+        Run `wf.py -f -n files` and return the output.
+        :param files: list of file contents.
+        :return: Program output with every file path replaced with a `#`.
+        """
+        tempfiles = []
+        for file in files:
+            tempfile = NamedTemporaryFile('w', delete=False, encoding='utf8')
+            tempfile.write(file)
+            tempfile.flush()
+            tempfiles.append(tempfile)
+        output = ''.join(main(['-f', '-x'] + [tempfile.name for tempfile in tempfiles]))
+        for tempfile in tempfiles:
+            output = output.replace(tempfile.name, '#')
+            tempfile.close()
+        return output
+
+    @staticmethod
+    def __format(data):
+        """
+        Format program output.
+        :param data: list of (item, count)s.
+        :return: str to be compared with.
+        """
+        return 'File: #\n' + ''.join('%40s\t%d\n' % i for i in data) + '\n'
+
+    def test_file(self):
+        output = self.__run('', '1', 'a', 'a b')
+        expect = self.__format([]) + self.__format([('a', 1)]) + self.__format([('a', 1), ('b', 1)])
+        self.assertEqual(output, expect)
+        output = self.__run('a', '1', 'a', 'a b')
+        expect = self.__format([]) + self.__format([]) + self.__format([('b', 1)])
+        self.assertEqual(output, expect)
+        output = self.__run('a\nc', '1', 'a', 'a b')
+        self.assertEqual(output, expect)
+
+
 if __name__ == '__main__':
     unittest.main()
--- wf.py
+++ wf.py
@@ -21,42 +21,54 @@ def letter(path):
         yield '%40s\t%d\n' % (k, v)
     yield '\n'
 
-    
-def word(path, limit):
+
+def word(path, limit, stopwords):
+    """
+    :param path:
+    :param limit:
+    :param stopwords: path
+    :return:
+    """
     counts = defaultdict(lambda: 0)
     with open(path, encoding='utf8') as f:
-        pattern = re.compile(r'[a-zA-Z0-9]+')  
+        pattern = re.compile(r'[a-zA-Z0-9]+')
         s = f.read()
-        result = pattern.findall(s)        
+        result = pattern.findall(s)
         for w in result:
             if w[0].isdigit():
                 continue
             w = w.lower()
             counts[w] += 1
-    yield 'File: %s\n' %path
-    result=sorted(counts.items(), key=lambda i: (-i[1], i[0]))
+    if stopwords is not None:
+        with open(stopwords, encoding='utf-8') as f:
+            for line in f:
+                counts.pop(line.strip().lower(), None)
+    yield 'File: %s\n' % path
+    result = sorted(counts.items(), key=lambda i: (-i[1], i[0]))
     if limit is not None:
         result = result[:limit]
     for (k, v) in result:
         yield '%40s\t%d\n' % (k, v)
-    yield '\n' 
+    yield '\n'
 
-        
+
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('paths', metavar='<path>', nargs='+', help='file to process')
     parser.add_argument('-c', dest='function', action='store_const', const='letter', help='count letter occurrences')
     parser.add_argument('-f', dest='function', action='store_const', const='word', help='count word occurrences')
-    parser.add_argument('-d', dest='function', action='store_const', const='dir_word', help='count word occurrences in specified directory')
+    parser.add_argument('-d', dest='function', action='store_const', const='dir_word',
+                        help='count word occurrences in specified directory')
     parser.add_argument('-s', dest='recursive', action='store_const', const=True, default=False, help='recursive')
     parser.add_argument('-n', dest='limit', metavar='N', type=int, help='output top N lines')
+    parser.add_argument('-x', dest='stopwords', metavar='<path>', help='stopwords file')
     args = parser.parse_args(args)
     if args.function == 'letter':
         for path in args.paths:
             yield from letter(path)
     elif args.function == 'word':
         for path in args.paths:
-            yield from word(path, args.limit)
+            yield from word(path, args.limit, args.stopwords)
     elif args.function == 'dir_word':
         if not args.recursive:
             for path in args.paths:
@@ -65,19 +77,16 @@ def main(args):
                     temp_path = os.path.join(path, r)
                     if os.path.isdir(temp_path):
                         continue
-                    yield from word(temp_path, args.limit)
+                    yield from word(temp_path, args.limit, args.stopwords)
         else:
             for path in args.paths:
                 for (p, _, l) in os.walk(path):
                     for i in l:
-                        yield from word(os.path.join(p, i), args.limit)
-                        
-
-    
-            
+                        yield from word(os.path.join(p, i), args.limit, args.stopwords)
 
 
 if __name__ == '__main__':
     from sys import argv
+
     for output in main(argv[1:]):
         print(output, end='')
