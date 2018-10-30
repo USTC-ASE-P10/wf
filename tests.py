1755baa50f1d0c6c912aae8872a2d96a12a6b78a
GGchencan <32196669+GGchencan@users.noreply.github.com>
2018-10-30 11:57:42 +0800

    almost finish step1

--- tests.py
+++ tests.py
@@ -15,7 +15,7 @@ class TestLetterOccurrences(unittest.TestCase):
         """
         tempfiles = []
         for file in files:
-            tempfile = NamedTemporaryFile('w')
+            tempfile = NamedTemporaryFile('w', delete=False, encoding='utf8')
             tempfile.write(file)
             tempfile.flush()
             tempfiles.append(tempfile)
@@ -59,6 +59,56 @@ class TestLetterOccurrences(unittest.TestCase):
                     output = self.__run(file)
                     expect = self.__format(data)
                     self.assertEqual(output, expect)
+
+
+class TestWordOccurrences(unittest.TestCase):
+    @staticmethod
+    def __run(*files):
+        """
+        Run `wf.py -f files` and return the output.
+        :param files: list of file contents.
+        :return: Program output with every file path replaced with a `#`.
+        """
+        tempfiles = []
+        for file in files:
+            tempfile = NamedTemporaryFile('w', delete=False, encoding='utf8')
+            tempfile.write(file)
+            tempfile.flush()
+            tempfiles.append(tempfile)
+        output = ''.join(main(['-f'] + [tempfile.name for tempfile in tempfiles]))
+        for tempfile in tempfiles:
+            output = output.replace(tempfile.name, '#')
+            tempfile.close()
+        return output
+
+    @staticmethod
+    def __format(data):
+        """
+        Format program output.
+        :param data: list of (item, count)s.
+        :return: str to be compared with.
+        """
+        return 'File: #\n' + ''.join('%40s\t%d\n' % i for i in data) + '\n'
+
+    def test_1_file(self):
+        for i in ('', 'a'):
+            with self.subTest(i=i):
+                output = self.__run(i)
+                expect = self.__format([(i, 1)] if i else [])
+                self.assertEqual(output, expect)
+
+    def test_2_files(self):
+        for i in ('', 'a', 'b'):
+            for j in ('', 'a', 'b'):
+                with self.subTest(i=i, j=j):
+                    output = self.__run(i, j)
+                    expect = self.__format([(i, 1)] if i else []) + self.__format([(j, 1)] if j else [])
+                    self.assertEqual(output, expect)
+
+    def test_function(self):
+        output = self.__run('12ab@ * ajfd9ii a7j00* acx 12 acx')
+        expect = self.__format([('acx', 2), ('a7j00', 1), ('ajfd9ii', 1)])
+        self.assertEqual(output, expect)
 
 
 if __name__ == '__main__':
--- wf.py
+++ wf.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 import argparse
+import re
 from collections import defaultdict
 
 
 def letter(path):
     counts = defaultdict(lambda: 0)
-    with open(path) as f:
+    with open(path, encoding='utf8') as f:
         while True:
             c = f.read(1)
             if not c:
@@ -19,15 +20,40 @@ def letter(path):
         yield '%40s\t%d\n' % (k, v)
     yield '\n'
 
+    
+def word(path):
+    counts = defaultdict(lambda: 0)
+    with open(path, encoding='utf8') as f:
+        pattern = re.compile(r'[a-zA-Z0-9]+')  
+        s = f.read()
+        result = pattern.findall(s)        
+        for w in result:
+            if w[0].isdigit():
+                continue
+            w = w.lower()
+            counts[w] += 1
+    yield 'File: %s\n' %path
+    for (k, v) in sorted(counts.items(), key=lambda i: (-i[1], i[0])):
+        yield '%40s\t%d\n' % (k, v)
+    yield '\n' 
+
+        
+        
+
 
 def main(args):
     parser = argparse.ArgumentParser()
     parser.add_argument('paths', metavar='<path>', nargs='+', help='file to process')
     parser.add_argument('-c', dest='function', action='store_const', const='letter', help='count letter occurrences')
+    parser.add_argument('-f', dest='function', action='store_const', const='word', help='count word occurrences')
     args = parser.parse_args(args)
     if args.function == 'letter':
         for path in args.paths:
             yield from letter(path)
+    elif args.function == 'word':
+        for path in args.paths:
+            yield from word(path)
+            
 
 
 if __name__ == '__main__':
