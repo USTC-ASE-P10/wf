d77000b3f32a16fc23c3a02f3f7c76ee379a9223
Hypercube <hypercube@0x01.me>
2018-10-31 15:40:52 +0800

    clear up arguments

--- wf.py
+++ wf.py
@@ -17,18 +17,15 @@ def letter(path):
                 continue
             counts[c.lower()] += 1
     yield 'File: %s\n' % path
-    for (k, v) in sorted(counts.items(), key=lambda i: (-i[1], i[0])):
+    result = sorted(counts.items(), key=lambda i: (-i[1], i[0]))
+    if limit is not None:
+        result = result[:limit]
+    for (k, v) in result:
         yield '%40s\t%d\n' % (k, v)
     yield '\n'
 
 
-def word(path, limit, stopwords):
-    """
-    :param path:
-    :param limit:
-    :param stopwords: path
-    :return:
-    """
+def word(path):
     counts = defaultdict(lambda: 0)
     with open(path, encoding='utf8') as f:
         pattern = re.compile(r'(?<![a-zA-Z0-9])[a-zA-Z][a-zA-Z0-9]*')
@@ -37,10 +34,8 @@ def word(path, limit, stopwords):
         for w in result:
             w = w.lower()
             counts[verbs.get(w, w)] += 1
-    if stopwords is not None:
-        with open(stopwords, encoding='utf-8') as f:
-            for line in f:
-                counts.pop(line.strip().lower(), None)
+    for w in stopwords:
+        counts.pop(w, None)
     yield 'File: %s\n' % path
     result = sorted(counts.items(), key=lambda i: (-i[1], i[0]))
     if limit is not None:
@@ -50,7 +45,7 @@ def word(path, limit, stopwords):
     yield '\n'
 
 
-def phrase(path, n):
+def phrase(path):
     counts = defaultdict(lambda: 0)
     with open(path, encoding='utf8') as f:
         pattern = re.compile(r'[a-zA-Z0-9\s]+')
@@ -60,29 +55,60 @@ def phrase(path, n):
             words = []
             for w in word_p.findall(sen):
                 w = w.lower()
-                words.append(verbs.get(w, w))
-            for i in range(len(words) - n + 1):
-                counts[tuple(words[i:i + n])] += 1
+                if w not in stopwords:
+                    words.append(verbs.get(w, w))
+            for i in range(len(words) - phrase_len + 1):
+                counts[tuple(words[i:i + phrase_len])] += 1
     yield 'File: %s\n' % path
     result = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+    if limit is not None:
+        result = result[:limit]
     for (k, v) in result:
         yield '%40s\t%d\n' % (' '.join(k), v)
     yield '\n'
 
 
+class PhraseAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, 'function', self.const)
+        setattr(namespace, self.dest, values)
+
+
 def main(args):
     parser = argparse.ArgumentParser()
-    parser.add_argument('paths', metavar='<path>', nargs='+', help='file to process')
-    parser.add_argument('-c', dest='function', action='store_const', const='letter', help='count letter occurrences')
-    parser.add_argument('-f', dest='function', action='store_const', const='word', help='count word occurrences')
-    parser.add_argument('-d', dest='function', action='store_const', const='dir_word',
-                        help='count word occurrences in specified directory')
-    parser.add_argument('-p', dest='function', metavar='N', type=int, help='count phrase (with N words) occurrences')
-    parser.add_argument('-s', dest='recursive', action='store_const', const=True, default=False, help='recursive')
-    parser.add_argument('-n', dest='limit', metavar='N', type=int, help='output top N lines')
-    parser.add_argument('-x', dest='stopwords', metavar='<path>', help='stopwords file')
-    parser.add_argument('-v', dest='verbs', metavar='<path>', help='verbs file')
+    parser.add_argument('paths', metavar='<path>', nargs='+',
+                        help='File to process.')
+    parser.add_argument('-f', dest='function', action='store_const', const=word,
+                        help='Count word occurrences.')
+    parser.add_argument('-c', dest='function', action='store_const', const=letter,
+                        help='Count character occurrences.')
+    parser.add_argument('-p', dest='phrase_len', action=PhraseAction, const=phrase, metavar='<len>', type=int,
+                        help='Count phrase occurrences, treating any <len> contiguous words as a phrase.')
+    parser.add_argument('-n', dest='limit', metavar='<num>', type=int,
+                        help='Output only the top <num> items.')
+    parser.add_argument('-x', dest='stopwords', metavar='<stop-words>',
+                        help='Use <stop-words> as a list of stop words, which are ignored in the counting.'
+                             ' Effective only with -f or -p specified.')
+    parser.add_argument('-v', dest='verbs', metavar='<verb-dict>',
+                        help='Use <verb-dict> as a verb dictionary that can be used with -p or -q.')
+    parser.add_argument('-d', dest='directory', action='store_true',
+                        help='Treat <path> as the path to a directory and operate on each file inside the directory.')
+    parser.add_argument('-s', dest='recursive', action='store_true',
+                        help='Recurse into sub-directories. Must be used with -d.')
     args = parser.parse_args(args)
+
+    global phrase_len
+    phrase_len = args.phrase_len
+
+    global limit
+    limit = args.limit
+
+    global stopwords
+    stopwords = set()
+    if args.stopwords is not None:
+        with open(args.stopwords, encoding='utf8') as f:
+            for line in f:
+                stopwords.add(line.strip().lower())
 
     global verbs
     verbs = {}
@@ -93,29 +119,15 @@ def main(args):
                 for w in words.split(','):
                     verbs[w.lower()] = verb.lower()
 
-    if args.function == 'letter':
-        for path in args.paths:
-            yield from letter(path)
-    elif args.function == 'word':
-        for path in args.paths:
-            yield from word(path, args.limit, args.stopwords)
-    elif args.function == 'dir_word':
-        if not args.recursive:
-            for path in args.paths:
-                result = os.listdir(path)
-                for r in result:
-                    temp_path = os.path.join(path, r)
-                    if os.path.isdir(temp_path):
-                        continue
-                    yield from word(temp_path, args.limit, args.stopwords)
+    for path in args.paths:
+        if args.directory:
+            for (p, _, l) in os.walk(path):
+                for i in l:
+                    yield from args.function(i)
+                if not args.recursive:
+                    break
         else:
-            for path in args.paths:
-                for (p, _, l) in os.walk(path):
-                    for i in l:
-                        yield from word(os.path.join(p, i), args.limit, args.stopwords)
-    else:
-        for path in args.paths:
-            yield from phrase(path, args.function)
+            yield from args.function(path)
 
 
 if __name__ == '__main__':
